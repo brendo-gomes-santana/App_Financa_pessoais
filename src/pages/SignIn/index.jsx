0b77b0f918d561d00f9e66cdae69e4789067bff2
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-08-17 16:09:08 -0300

    página logine cadastrado, criado já

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import { View, Text } from "react-native";
+import { Background, 
+            Container, 
+            Logo, 
+            AreaInput, 
+            Input, 
+            SubmitButton, 
+            SubmitText, 
+            Link, 
+            LinkText 
+        } from "./styled";
+        
+export default function SignIn() {
 
-export default function SignIn(){
-    return(
-        <View>
-            <Text>TELA LOGIN</Text>
-        </View>
+    const navigate = useNavigation();
+
+    return (
+        <Background>
+            <Container
+                behavior={Platform.OS === 'ios' && 'padding'}
+                enabled
+            >
+                <Logo
+                    source={require('../../assets/Logo.png')}
+                />
+                <AreaInput>
+                    <Input placeholder='seu email...' />
+                </AreaInput>
+                <AreaInput>
+                    <Input placeholder='Senha...' />
+                </AreaInput>
+
+                <SubmitButton activeOpacity={0.8}>
+                    <SubmitText>Acessar</SubmitText>
+                </SubmitButton>
+
+                <Link onPress={() => navigate.navigate('SignUp')}>
+                    <LinkText>Criar uma conta</LinkText>
+                </Link>
+            </Container>
+        </Background>
     )
 }
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { Background, 
+            Container, 
+            AreaInput, 
+            Input, 
+            SubmitButton, 
+            SubmitText 
+        } from '../SignIn/styled';
 
-export default function SignUp(){
-    return(
-        <View>
-            <Text>TELA CADASTRADP</Text>
-        </View>
+export default function SignUp() {
+    return (
+        <Background>
+            <Container>
+                <AreaInput>
+                    <Input placeholder="Nome"/>
+                </AreaInput>
+                <AreaInput>
+                    <Input placeholder="Seu Email"/>
+                </AreaInput>
+                <AreaInput>
+                    <Input placeholder="Sua Senha"/>
+                </AreaInput>
+                <SubmitButton>
+                    <SubmitText
+                        activeOpacity={0.8}
+                    >Cadastrar</SubmitText>
+                </SubmitButton>
+            </Container>
+        </Background>
     )
 }
